24b67e0133504c19e149c65df8f37e1d55f07dbe
Julian Pufler <puf17640@spengergasse.at>
2021-05-28 14:54:54 +0200

    Deploy Next.js to gh-pages

--- _next/static/cRa6Sq5wRZD9zhwhfAfXz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/00ab45bce88d36301c66.css","static/chunks/pages/index-f7499abda2c60aa5855c.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
