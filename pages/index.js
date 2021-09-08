d36774a02b8864ede22fb9a382f983ccd52b2545
Julian Pufler <julian.pufler@lean-coders.at>
2021-09-08 16:20:43 +0200

    Deploy Next.js to gh-pages

--- pages/index.js
+++ pages/index.js
@@ -64,6 +64,7 @@ export default function Home() {
         src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
         data-id="puf17640"
         data-description="Support me!"
+        data-message=""
         data-color="#ff813f"
         data-position="Right"
         data-x_margin="18"
