fe14cefb6ef933f76c8562edffa9a8a2cde24162
Julian Pufler <julian.pufler@lean-coders.at>
2021-12-06 16:06:52 +0100

    Deploy Next.js to gh-pages

--- out/_next/static/wRnrwHB5aZoAasVBr0dIt/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/2d7305676c8771e6f01d.css","static/chunks/pages/index-bae3d008e0992bc9c704.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
