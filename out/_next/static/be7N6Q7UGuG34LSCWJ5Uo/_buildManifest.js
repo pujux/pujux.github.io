70b75f61ecfc3f89411f650c1f5f6b33eebc2c82
Julian Pufler <julian.pufler@lean-coders.at>
2023-03-05 17:40:42 +0100

    remove old files

--- out/_next/static/be7N6Q7UGuG34LSCWJ5Uo/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/2d7305676c8771e6f01d.css","static/chunks/pages/index-8e8d8a470c8ca14d7660.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
