69846abef8b359d6ae7763e4b92590882d4f11d2
Julian Pufler <julian.pufler@lean-coders.at>
2023-05-21 00:29:32 +0200

    added umami tracker

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head></Head>
+      <Head>
+        <script
+          async
+          src='https://umami.pufler.dev/script.js'
+          data-website-id='b4b7a77d-7038-49a9-b828-54533e0286c7'
+        ></script>
+      </Head>
       <body>
         <Main />
 
